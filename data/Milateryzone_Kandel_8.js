85157365cf76d435f04d1977b666fee6a173c6b1
Elham163 <135461280+Elham163@users.noreply.github.com>
2024-07-15 11:36:20 +0200

    Add files via upload

---
+++ data/Milateryzone_Kandel_8.js
@@ -0,0 +1,1 @@
+var json_Milateryzone_Kandel_8 = {"type":"FeatureCollection","name":"Milateryzone_Kandel_8","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
